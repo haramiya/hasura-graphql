2075ea6eeaf5c60c6203e49637927b8309cf719c
Miyahara <haramiya21@gmail.com>
2022-04-29 21:02:19 +0900

    init and add Layout

---
+++ __test__/Home.test.tsx
@@ -0,0 +1,7 @@
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import Home from "../pages/index";
+
+it("Should render title text", () => {
+  render(<Home />);
+});
